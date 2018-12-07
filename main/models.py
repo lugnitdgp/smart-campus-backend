b537ab9a9b20267823489b077e27c621768fa840
Jayjeet Chakraborty <jc.github@rediffmail.com>
2018-12-07 15:02:23 +0530

    Added more fieldS

--- main/models.py
+++ main/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth import models as auth_models
 
 
 class User(auth_models.User):
-
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+    is_cr = models.BooleanField(default=False)
     roll_no = models.CharField(max_length=10)
 
     def __str__(self):
         return self.roll_no
+
